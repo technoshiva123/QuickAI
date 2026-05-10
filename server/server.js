a7a11ab7da1c25d8b7d18476ca044c2cd6e33b3f
technoshiva123 <147992386+technoshiva123@users.noreply.github.com>
2026-05-11 02:38:10 +0530

    Fix: Resolved PathError in CORS and stabilized server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 import 'dotenv/config'
-import exprss from 'express'
+import express from 'express' 
 import cors from 'cors'
 import { clerkMiddleware, requireAuth } from '@clerk/express'
 import aiRouter from './routes/aiRoutes.js'
 import connectCloudinary from './configs/cloudinary.js'
 import userRouter from './routes/userRoutes.js'
-const app = express()
 
+const app = express() 
 await connectCloudinary()
 
 app.use(cors({
@@ -14,22 +14,20 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(exprss.json())
+app.use(express.json()) 
 app.use(clerkMiddleware())
 
-app.get('/',(req,res)=>res.send('Server is Live!'))
+app.get('/', (req, res) => res.send('Server is Live!'))
 
-app.use(requireAuth())
-app.use('/api/ai',aiRouter)
-app.use('/api/user',userRouter)
-
+app.use('/api/ai', requireAuth(), aiRouter)
+app.use('/api/user', requireAuth(), userRouter)
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT,()=>{
-    console.log('Server is running on port',PORT);
-})
 
 app.all('(.*)', (req, res) => {
   res.status(404).send('Route not found');
 });
+
+app.listen(PORT, () => {
+    console.log('Server is running on port', PORT);
+})
